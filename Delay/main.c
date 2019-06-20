93332f37a9ed11dea0785f1c3b56dfe9bcab4f8a
ssssiot <privia@alumni.sjtu.edu.cn>
2019-06-20 23:07:15 +0800

    Add delay function

--- Delay/main.c
+++ Delay/main.c
@@ -1,16 +1,27 @@
 #include "stm8s.h"
+#include "delay.h"
 
 CLK_Source_TypeDef clk_source;
 u32 u32_clk_freq;
 
 void main(void)
 {
-  CLK_ClockSwitchConfig(CLK_SWITCHMODE_MANUAL, CLK_SOURCE_LSI, ENABLE, CLK_CURRENTCLOCKSTATE_DISABLE);
+  CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+  
+  delay_init(16);
 
   clk_source = CLK_GetSYSCLKSource();
   u32_clk_freq = CLK_GetClockFreq();
   
-  while (1);
+  GPIO_Init(GPIOD, GPIO_PIN_1, GPIO_MODE_OUT_PP_LOW_FAST);
+
+  while (1)
+  {
+    GPIOD->ODR |= GPIO_PIN_1;
+    delay_us(10);
+    GPIOD->ODR &= (~GPIO_PIN_1);
+    delay_us(10);
+  }
 }
 
 void assert_failed(u8* file, u32 line)
